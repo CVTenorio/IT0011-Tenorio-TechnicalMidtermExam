3ebcafe421310593658ca92ae0ec2846ede8259e
CVTenorio <202310471@fit.edu.ph>
2025-02-12 02:54:20 +0800

    add newline

--- Program1/tenorio_palindrome.py
+++ Program1/tenorio_palindrome.py
@@ -1,6 +1,6 @@
 # Carl Vincent Tenorio TW24 Palindrome
 """
-INSTRUCTION:
+INSTRUCTIONS:
 Create a program that will open the
 file numbers.txt and check each line if the sum
 of the given string digit numbers is palindrome
@@ -17,9 +17,9 @@ def processed_FileData(filename_daw):
                 num = list(map(int, line.strip().split(',')))
                 total_niya = sum(num)
                 if palindrome(total_niya):
-                    print(f"The sum {total_niya} is a palindrome daw!")
+                    print(f"The sum {total_niya} is a palindrome daw!\n")
                 else:
-                    print(f"The sum {total_niya} is not a palindrome daw!")
+                    print(f"The sum {total_niya} is not a palindrome daw!\n")
     except FileNotFoundError:
         print("\nFile not found. PLease check mo filename if nandyan and try again.")
     except ValueError:
@@ -27,17 +27,17 @@ def processed_FileData(filename_daw):
 
 def inputKa_numero():
     try:
-        num = list(map(int, input("Enter ka limang numero na nakahiwalay gamit ang commas: ").split(',')))
+        num = list(map(int, input("\nEnter ka limang numero na nakahiwalay gamit ang commas: ").split(',')))
         if len(num) !=5:
             print("\nMaglagay ka nang sakto na limang numero.")
             return
         total_niya = sum(num)
         if palindrome(total_niya):
-            print(f"The sum {total_niya} is a palindrome daw!")
+            print(f"\nThe sum {total_niya} is a palindrome daw!\n")
         else:
-            print(f"The sum {total_niya} hindi daw palindrome!")
+            print(f"\nThe sum {total_niya} hindi daw palindrome!\n")
     except ValueError:
-        print("\nInvalid daw ang nilagay mo. Maaari mo bang ulitin ang iyong nilagay. Maglagay lamang nang 5 numbers na nakahiwalay by commas.")
+        print("\nInvalid daw ang nilagay mo. Maaari mo bang ulitin ang iyong nilagay. Maglagay lamang nang 5 numbers na nakahiwalay by commas.\n")
 
 def main():
     filename_daw = "numbers.txt"
