dae954a95a2b8183b0f8d050c7bfd5997b32ee99
CVTenorio <202310471@fit.edu.ph>
2025-02-12 03:33:39 +0800

    sure done na to

--- Program1/tenorio_palindrome.py
+++ Program1/tenorio_palindrome.py
@@ -17,32 +17,17 @@ def processed_FileData(filename_daw):
                 num = list(map(int, line.strip().split(',')))
                 total_niya = sum(num)
                 if palindrome(total_niya):
-                    print(f"The sum {total_niya} is a palindrome daw!\n")
+                    print(f"The sum {total_niya} palindrome daw!\n")
                 else:
-                    print(f"The sum {total_niya} is not a palindrome daw!\n")
+                    print(f"The sum {total_niya} hindi daw palindrome!\n")
     except FileNotFoundError:
-        print("\nFile not found. PLease check mo filename if nandyan and try again.")
+        print("\nFile not found. Maaari mo bang tignan kung tama ang filename and try again.")
     except ValueError:
-        print("\nInvalid and data sa file. Please ensure the file contains comma-separated numebers. ")
-
-def inputKa_numero():
-    try:
-        num = list(map(int, input("Hello Sayo Kaibigan ko!\n""Maglagay ka nang limang numero na nakahiwalay gamit ang commas: ").split(',')))
-        if len(num) !=5:
-            print("\nMaglagay ka nang sakto na limang numero.")
-            return
-        total_niya = sum(num)
-        if palindrome(total_niya):
-            print(f"\nThe sum {total_niya} is a palindrome daw!\n")
-        else:
-            print(f"\nThe sum {total_niya} hindi daw palindrome!\n")
-    except ValueError:
-        print("\nInvalid daw ang nilagay mo. Maaari mo bang ulitin ang iyong nilagay. Maglagay lamang nang 5 numbers na nakahiwalay by commas.\n")
+        print("\nInvalid and data sa file. Please ensure the file contains comma-separated numbers. ")
 
 def main():
     filename_daw = "numbers.txt"
     processed_FileData (filename_daw)
-    inputKa_numero()
 
 if __name__ == "__main__":
     main()
